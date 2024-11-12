e024023d8a96f0b5e574291321cf70e1ef0ef2d5
DokerMW <abddoker@gmail.com>
2024-11-12 20:54:01 +0500

    fix: deleted unnecessary files

--- _src/js/script.js
+++ _src/js/script.js
@@ -1,8 +1,6 @@
 //@prepros-append _dynamic_adapt.js
 //@prepros-append _burger.js
-//@prepros-append _sliders.js
 //@prepros-append _accordion.js
-//@prepros-append _tabs.js
 //@prepros-append _popup.js
 //@prepros-append _mirror.js
 //@prepros-append _validation.js
